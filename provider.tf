d9190e999994af9b95fab84ebb5bd04634b73858
vinay <vinaybkrishna10@gmail.com>
2025-05-22 21:35:48 +0530

    Commmmiting mmy first commit

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1" # change if needed
 }
 provider "vault" {
-  address = "http://44.203.215.181:8200/"
+  address = "http://vault.mikeydevops1.online:8200/"
   token = var.vault_token
 }
