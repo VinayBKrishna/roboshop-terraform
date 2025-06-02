28543a5450980093e9ca61b21ea40eaa8a2ef463
vinay <vinaybkrishna10@gmail.com>
2025-06-02 22:58:56 +0530

    Commmmiting mmy first commit

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -19,7 +19,7 @@ resource "aws_eks_cluster" "example" {
 resource "aws_eks_node_group" "main" {
   for_each = var.node_groups
   cluster_name    = aws_eks_cluster.example.name
-  node_group_name = each.name
+  node_group_name = each.key
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnets
   instance_types = each.value["instance_type"]
